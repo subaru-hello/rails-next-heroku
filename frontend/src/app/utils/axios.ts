9e5bfdbc78d75616a7fefe46da27e7ecfedebf7e
subaru-hello <octosubaru0926@gmail.com>
2023-05-17 22:23:03 +0900

    endpoint修正

--- frontend/src/app/utils/axios.ts
+++ frontend/src/app/utils/axios.ts
@@ -2,5 +2,4 @@ import axios from "axios";
 
 export const axiosInstance = axios.create();
 // axiosInstance.defaults.baseURL = process.env.REACT_APP_API_DOMEIN;
-// axiosInstance.defaults.baseURL = "https://rails-next-heroku-lwuoc6jsw-subaru-hello.vercel.app"
-axiosInstance.defaults.baseURL = "https://todo202305142200.herokuapp.com/"
+axiosInstance.defaults.baseURL = "https://todo202305142200.herokuapp.com"
